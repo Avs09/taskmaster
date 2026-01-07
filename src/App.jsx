6dbd76d6bb1bcc0d0a6cadc2dc37d62e9e07dd69
Alexander <suarezalex596@gmail.com>
2026-01-07 14:26:42 -0500

    feat: protect routes with require auth

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Home from './pages/Home'
 import Login from './pages/Login'
+import RequireAuth from './components/RequireAuth'
 
 function App() {
   return (
@@ -11,7 +12,14 @@ function App() {
 
         <main className="max-w-5xl mx-auto px-4 py-6">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route
+              path="/"
+              element={
+                <RequireAuth>
+                  <Home />
+                </RequireAuth>
+              }
+            />
             <Route path="/login" element={<Login />} />
           </Routes>
         </main>
